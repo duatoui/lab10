4db54b9400bd031161490a0d90e7dc45bb3c94dd
Dua Atoui <duatoui@umich.edu>
2021-04-15 15:21:48 -0400

    changes to testSelectionSort

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -11,25 +11,24 @@ public class testSelectionSort {
 		testDuplicates();
 	}
 
-    public testSelectionSort() {
-    }
+  public void testMixed(){
 
-    public void testPositive(){
-        int[] arr = new int[5];
-        arr[0] = 8;
-        arr[1] = 9;
-        arr[2] = 7;
-        arr[3] = 10;
-        arr[4] = 2;
+    	int[] arr = new int[5];
+		arr[0] = 8;
+		arr[1] = 9;
+		arr[2] = 7;
+		arr[3] = -10;
+		arr[4] = -2;
 
-        int[] Sortedarr = new int[5];
-        Sortedarr[0] = 2;
-        Sortedarr[1] = 7;
-        Sortedarr[2] = 8;
-        Sortedarr[3] = 9;
-        Sortedarr[4] = 10;
+		int[] Sortedarr = new int[5];
 
-        SelectionSort sort = new SelectionSort();
+		Sortedarr[0] = -10;
+		Sortedarr[1] = -2;
+		Sortedarr[2] = 7;
+		Sortedarr[3] = 8;
+		Sortedarr[4] = 9;
+
+		SelectionSort sort = new SelectionSort();
 
 		arr = sort.basicSelectionSort(arr);
 
@@ -38,33 +37,30 @@ public class testSelectionSort {
 			assertTrue("Element sorted",arr[i] == Sortedarr[i]);
 		}
 	}
-
-
-
-    public void testNegative(){
+    public void testDuplicates(){
 
     	int[] arr = new int[5];
-		arr[0] = -8;
-		arr[1] = -9;
-		arr[2] = -7;
-		arr[3] = -10;
-		arr[4] = -2;
+		arr[0] = 8;
+		arr[1] = 8;
+		arr[2] = 7;
+		arr[3] = 10;
+		arr[4] = 2;
 
 		int[] Sortedarr = new int[5];
 
-		Sortedarr[0] = -10;
-		Sortedarr[1] = -9;
-		Sortedarr[2] = -8;
-		Sortedarr[3] = -7;
-		Sortedarr[4] = -2;
+		Sortedarr[0] = 2;
+		Sortedarr[1] = 7;
+		Sortedarr[2] = 8;
+		Sortedarr[3] = 8;
+		Sortedarr[4] = 10;
 
 		SelectionSort sort = new SelectionSort();
+
 		arr = sort.basicSelectionSort(arr);
 
-for (int i = 0; i < 5; i++)
+		for (int i = 0; i < 5; i++)
 		{
-			assertTrue("Element sorted",arr[i] == Sortedarr[i]);
+					assertTrue("Element sorted",arr[i] == Sortedarr[i]);
 		}
-	}
-
-    
+    }
+}
